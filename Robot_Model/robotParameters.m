b78d477c2db689ab690e166013425a5b0fdd70e1
Mikhail Russell <RSSMIK001@myuct.ac.za>
2022-08-18 10:30:21 +0200

    Fixed param

--- Robot_Model/robotParameters.m
+++ Robot_Model/robotParameters.m
@@ -4,5 +4,5 @@ axleLength = 0.136; %Distance between wheels (m)
 ticksPerRot = 20; % Ticks per rotation for encoders
 load wheelLUT %Look up table for platform motors
 Ts = 0.01; %Sample time
-load wheelMotorModel %Motor model for platform motorswheelMotorModel
-load straigh_line.mat
+%load wheelMotorModel %Motor model for platform motorswheelMotorModel
+%load straigh_line.mat
